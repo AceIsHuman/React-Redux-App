d62da11c17af21e523e3642707956ba09b267150
AceIsHuman <menoman1995@yahoo.com>
2019-09-26 21:28:15 -0500

    create playerReducer

---
+++ clash-of-clans-redux/src/reducers/playerReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  status: "Player Not Loaded",
+  playerData: {},
+  isLoading: false
+};
+
+export const playerReducer = (state = initialState, action) => {
+  switch(action.type){
+    default: state;
+  }
+}
